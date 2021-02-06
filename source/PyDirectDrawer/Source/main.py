ffb923f9b95a000f4a0ca46a70b8edb358ad2dca
Alex Crush <contr239@mail.ru>
2021-02-06 18:22:26 +0300

    ! SURV-12 Наброски алгоритма отображения завершенности вычисления(readiness)

--- source/PyDirectDrawer/Source/main.py
+++ source/PyDirectDrawer/Source/main.py
@@ -1,8 +1,9 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.colors as mcolors
+#from matplotlib.patches import StepPatch
 import pandas as pd
-import numpy as np
 import json
 import sys
 import os
@@ -36,11 +37,14 @@ class LogValueToken():
     LVT_UNIT_COUNT  = 'unit_count'
 
 class Plotter:
+    unit_count = 0
     fig = {}
     ax_gnt = {}
     label_pool = []
     color_palette = []
     vlines_cluster = dict()
+    shard_count = []
+    shard_status = []
     
     def get_bw_color(self, color):
         if (color[0] + color[1] + color[2] > 1.5):
@@ -52,15 +56,15 @@ class Plotter:
         return color_names
 
     def prepare_plot_template(self):
-        self.fig, self.ax_gnt = plt.subplots(figsize = (12, 8))
+        self.fig, self.ax_gnt = plt.subplots(1, 2, figsize = (12, 8))
 
     def __init__(self):
         pass
         #self.service = self.Service()
 
     def plot_finisher(self, unitCount, pathLength, packetSize, threadCount):
-        self.ax_gnt.grid(True)
-        self.ax_gnt.legend()
+        self.ax_gnt[0].grid(True)
+        self.ax_gnt[0].legend()
         
         self.fig.suptitle('Experiment for: ' + str(unitCount) + ' units, ' + str(pathLength) + ' path length, ' + str(packetSize) + ' tasks packet size, ' + str(threadCount) + ' thread count')
         self.fig.autofmt_xdate() 
@@ -75,7 +79,7 @@ class Plotter:
         interval = (shard_data["start_ts"], shard_data["finish_ts"] - shard_data["start_ts"])
         color = self.color_palette[shard_data["unit_idx"]]
         idx = shard_data["holder_idx"]
-        bar = self.ax_gnt.broken_barh(
+        bar = self.ax_gnt[0].broken_barh(
             [interval],
             (idx*(CELL_HEIGHT + 1), CELL_HEIGHT),
             facecolors = color,
@@ -85,7 +89,7 @@ class Plotter:
         center_x = interval[0] + interval[1]/2
         center_y = idx*(CELL_HEIGHT + 1) + CELL_HEIGHT/2
 
-        self.ax_gnt.text(
+        self.ax_gnt[0].text(
             x = center_x, 
             y = center_y,
             s = "u:" + str(shard_data["unit_idx"]) + " s:" + str(shard_data["shard_idx"]), 
@@ -102,6 +106,49 @@ class Plotter:
         #cl_item = self.vlines_cluster.get(shard_data["packet_idx"])
         #if (shard_data["packet_idx"])
 
+    def analyze_shard_count(self, hcrh):
+        self.shard_count = np.zeros(self.unit_count)
+        mmax = 0
+        for rshard in hcrh[LogLevelToken.LLT_SUB_DATA]:
+            hcrd = shard[LogLevelToken.LLT_PACKET]
+            for pshard in hcrd[LogLevelToken.LLT_SUB_DATA]: 
+                task = pshard[LogLevelToken.LLT_TASK]
+                self.shard_count[task.unit_idx] = task.shard_idx + 1 if (self.shard_count[task.unit_idx] < task.shard_idx + 1) else pass
+                mmax = task.shard_idx + 1 if (mmax < task.shard_idx + 1) else pass
+                    
+        self.shard_status = np.zeros((self.unit_count, mmax))
+
+    def commit_readiness(self):
+        pass
+
+    def build_readiness_stairs(self):
+        #A = [[0, 0, 0],
+            #[1, 2, 3],
+            #[2, 4, 6],
+            #[3, 6, 9]]
+        #for i in range(len(A) - 1):
+            #self.ax_gnt[1].stairs(A[i+1], baseline=A[i], fill=False)
+
+        x = np.arange(14)
+        y = np.sin(x / 2)
+
+        plt.step(x, y + 2, label='pre (default)')
+        plt.plot(x, y + 2, 'C0o', alpha=0.5)
+
+        plt.step(x, y + 1, where='mid', label='mid')
+        plt.plot(x, y + 1, 'C1o', alpha=0.5)
+
+        plt.step(x, y, where='post', label='post')
+        plt.plot(x, y, 'C2o', alpha=0.5)
+        
+
+    def plot_gantt_chart(self, log):
+        self.prepare_plot_template() 
+        self.parse_pipeline_log(log)
+        self.build_readiness_stairs()
+        
+        self.plot_finisher(self.unit_count, 0, 0, 0)
+
     def parse_task_log(self, hrd):
         #for shard in hrd[LogLevelToken.LLT_SUB_DATA]:
             #stat_unit = shard[LogLevelToken.LLT_TASK]
@@ -110,25 +157,28 @@ class Plotter:
     def parse_packet_log(self, hcrd):
         for shard in hcrd[LogLevelToken.LLT_SUB_DATA]: 
             self.parse_task_log(shard[LogLevelToken.LLT_TASK])
-
+        self.commit_readiness()
     #def draw_vlines(self, hcrd):
         #self.vlines_cluster
 
     def parse_run_log(self, hcrh):
+        self.unit_count = hcrh[LogValueToken.LVT_UNIT_COUNT]
+        self.analyze_shard_count(hcrh)
         for shard in hcrh[LogLevelToken.LLT_SUB_DATA]:
-            self.color_palette = self.allocate_color_palette(hcrh[LogValueToken.LVT_UNIT_COUNT])
-            self.prepare_plot_template() 
+            self.color_palette = self.allocate_color_palette(hcrh[LogValueToken.LVT_UNIT_COUNT])            
             self.parse_packet_log(shard[LogLevelToken.LLT_PACKET])
             #self.draw_vlines()
-            self.plot_finisher(hcrh[LogValueToken.LVT_UNIT_COUNT], 0, 0, 0)
+            
 
     def parse_pipeline_log(self, exh):
+        assert(len(exh[LogLevelToken.LLT_SUB_HISTORY]) == 1)
         for shard in exh[LogLevelToken.LLT_SUB_HISTORY]:
             self.parse_run_log(shard[LogLevelToken.LLT_RUN])
 
     def parse_overall_log(self, exh):
         for shard in exh[LogLevelToken.LLT_SUB_HISTORY]:
-            self.parse_pipeline_log(shard[LogLevelToken.LLT_PIPELINE])
+            self.plot_gantt_chart(shard[LogLevelToken.LLT_PIPELINE])
+            #self.parse_pipeline_log(shard[LogLevelToken.LLT_PIPELINE])
             
     def run(self, data_file_path):
         with open(data_file_path) as json_file:
